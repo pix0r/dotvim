035c5125c6a99063f97e7723028ef27c3cd3eee6
Mike Matz <mike@pixor.net>
2012-06-18 07:23:37 -0700

    Update install.sh to use symink to vimrc

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ REPO=http://github.com/pix0r/dotvim.git
 cd $HOME
 if [ ! -d ".vim" ]; then
 	git clone --recursive $REPO .vim
-	cp .vim/vimrc .vimrc
+	ln -s .vim/vimrc .vimrc
 else
 	echo "Error: $HOME/.vim already exists"
 	exit 1
